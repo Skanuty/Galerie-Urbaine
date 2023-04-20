0de3cafd2cc4f25c39a790754f6be34839ec9d04
Skanuty <98880821+Skanuty@users.noreply.github.com>
2023-04-20 22:02:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const paragraphe = document.getElementById('paragraphe');
     paragraphe.classList.toggle('full'); /* ajoute ou retire la classe full */
     boutonAfficher.innerHTML = paragraphe.classList.contains('full') ? '<strong>Afficher moins</strong>' : '<strong>Afficher plus</strong>'; /* change le texte du bouton en fonction de l'état du paragraphe */
   });
+
+var modal = document.querySelector('.mamodal')
+
+var button = document.querySelector('#modal-button')
+
+var close = document.querySelector('.close')
+
+button.onclick = function(){
+    modal.style.display="block"
+    close.style.display='block'
+}
+
+close.onclick = function(){
+    modal.style.display='none'
+}
+
+close.onclick = function(){
+    close.style.display='none'
+    modal.style.display='none'
+}
